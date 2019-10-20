37ad68d790feb69da52e57fba5ef9dc1893d73a1
Ioannis Gabrielides <ioannisgabrielides@gmail.com>
2019-10-20 21:27:17 +0100

    change seed

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ import tkinter as tk
 from random import seed, choice
 from string import ascii_letters
 
-seed(63)
+seed(84)
 
 colors = ('red', 'yellow', 'green', 'cyan', 'blue', 'magenta')
 def do_stuff():
